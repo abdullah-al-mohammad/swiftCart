7d2e7a709391c60fde6cc4e4255ce67ee8d8ec67
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-02-17 12:09:59 +0600

    add product details functionality and enhance product display

--- products/script.js
+++ products/script.js
@@ -5,6 +5,13 @@ const loadProducts = async () => {
   displayProducts(products);
 };
 
+const productDetails = id => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetail(data));
+};
+
 const loadCategories = async () => {
   const url = 'https://fakestoreapi.com/products/categories';
   const res = await fetch(url);
@@ -13,9 +20,8 @@ const loadCategories = async () => {
 };
 
 const removeActive = () => {
-  const lessonButtons = document.querySelectorAll('.category-btn');
-  lessonButtons.forEach(btn => btn.classList.remove('active'));
-  console.log(lessonButtons);
+  const categoryButtons = document.querySelectorAll('.category-btn');
+  categoryButtons.forEach(btn => btn.classList.remove('active'));
 };
 
 const loadCategory = async category => {
@@ -26,9 +32,33 @@ const loadCategory = async category => {
   const clickBtn = document.getElementById(`category-btn-${category}`);
   clickBtn.classList.add('active');
   displayProducts(categoryProducts);
-  console.log(categoryProducts, 'category');
 };
 loadCategory();
+
+const displayDetail = product => {
+  const detailsBox = document.getElementById('detail-container');
+  detailsBox.innerHTML = `
+  <div>
+    <h2 class="text-xl font-bold">
+      <span class="text-blue-600">Title:</span> ${product.title}
+    </h2>
+  </div>
+  <div>
+    <h2 class="text-lg font-bold">Description</h2>
+    <p>${product.description}</p>
+  </div>
+  <div>
+    <p><span>rating:</span> <span class="text-yellow-500">${product.rating.rate}</span> (${product.rating.count})</p>
+  </div>
+  <div>
+    <p><span>price:</span> $${product.price}</p>
+  </div>
+  <div>
+    <button class="btn btn-primary" type="button">Buy Now</button>
+  </div>
+  `;
+  document.getElementById('detail_modal').showModal();
+};
 
 const displayCategories = categories => {
   const categoriesContainer = document.getElementById('product-category');
@@ -79,8 +109,6 @@ loadCategories();
 // }
 
 const displayProducts = products => {
-  console.log(products);
-
   const productsContainer = document.getElementById('products');
   productsContainer.innerHTML = '';
   productsContainer.classList.add('grid', 'grid-cols-1', 'md:grid-cols-3', 'gap-5');
@@ -88,21 +116,21 @@ const displayProducts = products => {
     const productDiv = document.createElement('div');
     productDiv.innerHTML = `
 <div class="card bg-base-100 shadow-sm">
-    <figure>
-      <img class="h-40"
+    <figure class="bg-slate-400">
+      <img class="h-80"
         src="${p.image}"
         alt="${p.category}"
       />
     </figure>
     <div class="card-body">
       <div class="flex justify-between items-center">
-        <span>${p.category}</span>
+        <span class="bg-slate-400 rounded-full">${p.category}</span>
         <span><i class="fa-solid fa-star text-yellow-500"></i> ${p.rating.rate} (${p.rating.count})</span>
       </div>
-      <h2 class="card-title">${p.title}</h2>
+      <h2 class="card-title h-10">${p.title}</h2>
       <p>$${p.price}</p>
       <div class="card-actions justify-between">
-        <button class="btn btn-outline"><i class="fa-regular fa-eye"></i> Details</button>
+        <button onClick="productDetails(${p.id})" class="btn btn-outline"><i class="fa-regular fa-eye"></i> Details</button>
         <button class="btn btn-primary">
           <i class="fa-solid fa-cart-shopping"></i> Add
         </button>
